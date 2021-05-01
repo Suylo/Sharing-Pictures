149c096ef824dffc1c7f0727f4d2c0c1fe27fbeb
Suylo <spoownz@yahoo.com>
2021-05-01 13:44:02 +0200

    ✏ Href edited for URL rewriting

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -8,7 +8,7 @@ use App\models\UserAuth;
     <div class="searchHead">
         <form method="get" action="?q=search">
             <input type="search" name="query" id="search" placeholder="Rechercher...">
-	        <button type="submit" name="q" value="search"><i class="bi bi-search"></i></button>
+	        <button type="submit" name="q" value="search" class="input_icons"><i class="bi bi-search"></i></button>
         </form>
     </div>
     <nav>
@@ -18,11 +18,11 @@ use App\models\UserAuth;
             if (UserAuth::getInstance()->userIsLoggedOn()) {
                 include 'app/controllers/header.php';?>
 
-	            <li><a href="?q=profile&UID=<?= $userID ?>"><i class="bi bi-person"></i>Bienvenue <span class="bold"><?= $firstname ?></span></a></li>
-	            <li><a href="?q=logout" class="userLogout">Se déconnecter</a></li>
+	            <li><a href="./user-<?= $userID ?>"><i class="bi bi-person"></i>Bienvenue <span class="bold"><?= $firstname ?></span></a></li>
+	            <li><a href="./logout" class="userLogout">Se déconnecter</a></li>
 	        <?php } else { ?>
-	            <li><a href="?q=login"><i class="bi bi-person"></i> Connexion</a></li>
-	            <li><a href="?q=register"><i class="bi bi-person-plus-fill"></i> Inscription</a></li>
+	            <li><a href="./login"><i class="bi bi-person"></i> Connexion</a></li>
+	            <li><a href="./register"><i class="bi bi-person-plus-fill"></i> Inscription</a></li>
 	        <?php } ?>
         </ul>
     </nav>
@@ -33,15 +33,15 @@ use App\models\UserAuth;
 		<?php if (UserAuth::getInstance()->userIsLoggedOn()) { ?>
 
 
-			<li><a href="?q=home"><i class="bi bi-house-door"></i> Accueil</a></li>
-			<li><a href="?q=gallery"><i class="bi bi-images"></i> Galerie</a></li>
+			<li><a href="./home"><i class="bi bi-house-door"></i> Accueil</a></li>
+			<li><a href="./gallery"><i class="bi bi-images"></i> Galerie</a></li>
 
-			<li><a href="?q=profile&UID=<?= $userID ?>#my-pictures"><i class="bi bi-file-image"></i> Mes Photos</a></li>
-			<li><a href="?q=groups"><i class="bi bi-collection"></i> Groupes</a></li>
-			<li><a href="?q=settings"><i class="bi bi-gear"></i> Paramètres</a></li>
+			<li><a href="./user-<?= $userID ?>#my-pictures"><i class="bi bi-file-image"></i> Mes Photos</a></li>
+			<li><a href="./groups"><i class="bi bi-collection"></i> Groupes</a></li>
+			<li><a href="./settings"><i class="bi bi-gear"></i> Paramètres</a></li>
 		<?php } else { ?>
-			<li><a href="?q=home"><i class="bi bi-house-door"></i> Accueil</a></li>
-			<li><a href="?q=gallery"><i class="bi bi-images"></i> Galerie</a></li>
+			<li><a href="./home"><i class="bi bi-house-door"></i> Accueil</a></li>
+			<li><a href="./gallery"><i class="bi bi-images"></i> Galerie</a></li>
 		<?php } ?>
 	</ul>
 </aside>
